fa18d8854d73f77a6abe3932e9d19995ce17ae20
mahativedula <nsc3sj@virginia.edu>
2025-10-31 12:52:52 -0400

    here is how to connect to phpgsql database, edit index.php and db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -5,9 +5,9 @@ if (session_status() === PHP_SESSION_NONE) {
 
 try {
     $db = new PDO(
-        "pgsql:host=db;port=5432;dbname=invitationapp_db",
-        "localuser",
-        "cs4640LocalUser!",
+        "pgsql:host=localhost;port=5432;dbname=COMPUTING_ID_HERE",
+        "COMPUTING_ID_HERE",
+        "phpgsql_password_here",
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
 } catch (PDOException $e) {
